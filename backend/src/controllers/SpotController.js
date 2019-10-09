bf9a40488ba5cc30fc291722c43f30fc7407ca72
anacavalcanti <anabeatrizaac@gmail.com>
2019-10-09 16:57:15 -0300

    [Aircnc/backend] filename

--- backend/src/controllers/SpotController.js
+++ backend/src/controllers/SpotController.js
@@ -2,8 +2,7 @@ const Spot = require('../models/Spot');
 
 module.exports = {
     async store(req, res) {
-        console.log(req.body);
-        console.log(req.file);
+        const { filename } = req.file;
         return res.json({ ok: true });
     }
 };
